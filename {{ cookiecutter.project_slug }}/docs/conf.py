ff09ffd5ebac52f60a615c45463f24610266dbed
Chris Churas <churas.camera@gmail.com>
2023-01-26 10:34:19 -0800

    made copyright visible again

--- {{ cookiecutter.project_slug }}/docs/conf.py
+++ {{ cookiecutter.project_slug }}/docs/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'{{ cookiecutter.project_name }}'
-copyright = u"{% now 'local', '%Y' %}, {{ cookiecutter._copyright }}"
+copyright = u"{% now 'local', '%Y' %}, {{ cookiecutter.copyright }}"
 author = u"{{ cookiecutter.full_name }}"
 
 # The version info for the project you're documenting, acts as replacement
