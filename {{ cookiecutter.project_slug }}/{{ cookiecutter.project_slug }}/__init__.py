0bb2629cef4261255f08a799d29fc2e3e10bb07d
Chris Churas <churas.camera@gmail.com>
2025-03-26 15:01:35 -0700

    added author full name and computation name

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -2,8 +2,9 @@
 
 """Top-level package for {{ cookiecutter.project_name }}."""
 
-__author__ = """{{ cookiecutter.full_name }}"""
+__author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
 __repo_url__ = 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}'
 __description__ = '{{ cookiecutter.project_short_description }}'
+__computation_name__ = '{{ cookiecutter.computation_name }}'
